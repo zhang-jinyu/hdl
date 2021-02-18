94dc7b04723bc6b1b007ed1cbde92e03f0fe4400
Laszlo Nagy <laszlo.nagy@analog.com>
2021-02-18 14:37:27 +0000

    ad_quadmxfe1_ebz: Update ad_iobuf path

--- projects/ad_quadmxfe1_ebz/vcu118/system_project.tcl
+++ projects/ad_quadmxfe1_ebz/vcu118/system_project.tcl
@@ -56,7 +56,7 @@ adi_project_files ad_quadmxfe1_ebz_vcu118 [list \
   "timing_constr.xdc" \
   "../common/quad_mxfe_gpio_mux.v" \
   "../../../library/common/ad_3w_spi.v" \
-  "$ad_hdl_dir/library/xilinx/common/ad_iobuf.v" \
+  "$ad_hdl_dir/library/common/ad_iobuf.v" \
   "$ad_hdl_dir/projects/common/vcu118/vcu118_system_constr.xdc" ]
 
 set_property strategy Performance_Explore [get_runs impl_1]
